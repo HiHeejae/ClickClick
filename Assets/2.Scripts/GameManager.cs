749abb218bc2195f5ea9c090f6676c0abb3eef9b
Hiheejae <aag391345@gmail.com>
2024-04-12 16:14:29 +0900

    클클대8

--- Assets/2.Scripts/GameManager.cs
+++ Assets/2.Scripts/GameManager.cs
@@ -1,8 +1,10 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.SocialPlatforms.Impl;
 
 public class GameManager : MonoBehaviour
@@ -12,9 +14,23 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private int maxScore;
     [SerializeField] private int noteGroupCreateScore = 10;
+    [SerializeField] private GameObject gameclearobj;
+    [SerializeField] private GameObject gameoverobj;
+
     private int score;
     private int nextNoteGroupUnlockCnt;
     [SerializeField] private float maxtime = 30f;
+
+    public bool isGameDone
+    {
+        get
+        {
+            if (gameclearobj.activeSelf || gameoverobj.activeSelf)
+                return true;
+            else
+                return false;
+        }
+    }
 
     public void CalculateScore(bool isApple)
     {
@@ -28,12 +44,19 @@ public class GameManager : MonoBehaviour
                 nextNoteGroupUnlockCnt = 0;
                 NoteManager.Instance.CreateNoteGroup();
             }
+
+            if (maxScore <= score)
+            {
+                gameclearobj.SetActive(true);
+            }
         }
         else
         {
             score--;
         }
         UIManager.Instance.OnScoreChange(score, maxScore);
+
+        
     }
 
     private void Awake()
@@ -46,6 +69,9 @@ public class GameManager : MonoBehaviour
         UIManager.Instance.OnScoreChange(this.score, maxScore);
         NoteManager.Instance.Create();
 
+        gameclearobj.SetActive(false);
+        gameoverobj.SetActive(false);
+
         StartCoroutine(TimerCouroutine());
     }
 
@@ -57,9 +83,22 @@ public class GameManager : MonoBehaviour
             currentTime += Time.deltaTime;
             UIManager.Instance.OnTimerChange(currentTime, maxtime);
             yield return null;
+
+            if (isGameDone)
+            {
+                yield break;
+            }
         }
         Debug.Log("Game Over..");
+
+        gameoverobj.SetActive(true);
+        
+        
         
     }
-
+    public void Restart()
+    {
+        Debug.Log("Game Restart!");
+        SceneManager.LoadScene(0);
+    }
 }
